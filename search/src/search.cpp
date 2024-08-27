e31a3bd27da63ef44b017867666fe9c66871b067
TitoDiazH <amdiaz3@miuandes.cl>
2024-08-27 18:01:18 -0400

    agregue la parte de busqueda binaria

--- search/src/search.cpp
+++ search/src/search.cpp
@@ -29,8 +29,59 @@ int busquedaSecuencial(const vector<string>& array_words, const string& key) {
 }
 
 int busquedaBinaria(const vector<string>& array_words, const string& key) {
-    return 5;
+    int count = 0;
+    vector<string> copy_array_words = array_words;
+    quicksort(copy_array_words, 0, array_words.size());
+    int low = 0;
+    int high = copy_array_words.size(); 
+
+    while(low <= high){
+        int mitad = (low+high)/2;
+        if(copy_array_words[mitad] == key){
+            count++;
+        }
+        if(copy_array_words[mitad] > key){
+            high = mitad;
+            mitad = (low+high)/2;
+        }
+        if(copy_array_words[mitad] < key){
+            low = mitad;
+            mitad = (low+high)/2;
+        }
+    }
+    return count;
 }
+
+void swap(string &a, string &b){
+    string temp = a;
+    a = b;
+    b = temp;
+}
+
+int partition(vector<string> &arr, int low, int high){
+    string pivot = arr[high];
+    int i = low - 1;
+
+    for(int j = low; j <= high - 1; j++){
+        if(arr[j] < pivot){
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i+1], arr[high]);
+    return i+1;
+}
+
+void quicksort(vector<string> &arr, int low, int high){
+    if (low < high){
+        int pi = partition(arr, low, pi-1);
+        quicksort(arr,low,pi-1);
+        quicksort(arr,pi+1,high);
+    }
+}
+
+
+
 
 
 

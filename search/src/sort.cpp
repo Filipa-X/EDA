bef9d4b014146a0aa7442417f2f64a4826993f2c
TitoDiazH <amdiaz3@miuandes.cl>
2024-08-29 14:25:14 -0400

    a

--- search/src/sort.cpp
+++ search/src/sort.cpp
@@ -1,37 +1,36 @@
 #include <iostream>
+#include <fstream>
 #include "search/sort.hpp"
 #include "search/utils.hpp"
 using namespace std;
-
-namespace sort{
 	
-    int split_qs(string* A, int i, int j){
-		/***
-		 * split for quicksort
-		 * i,j are the endpoints
-		 */
-		int p = getRandomInt(i, j);
+int split_qs(string* A, int i, int j){
+	/***
+	 * split for quicksort
+	 * i,j are the endpoints
+	 */
+	int p = getRandomInt(i, j);
 
-		while (i  < j) {
+	while (i  < j) {
 
-			while ( i < p && A[i] <= A[p]){
-				i = i + 1;
-			}
-
-			while ( j > p && A[j] >= A[p]){
-				j = j - 1;
-			}
-
-			swap(A, i, j);
-
-			if (i == p){
-				p = j;
-			}
-			else if (j == p){
-				p = i;
-			}
+		while ( i < p && A[i] <= A[p]){
+			i = i + 1;
 		}
-		return p;
+
+		while ( j > p && A[j] >= A[p]){
+			j = j - 1;
+		}
+
+		swap(A, i, j);
+
+		if (i == p){
+			p = j;
+		}
+		else if (j == p){
+			p = i;
+		}
+	}
+	return p;
 	}
 
 	void quickSort(string* A, int i, int j){
@@ -47,4 +46,3 @@ namespace sort{
 		quickSort(A, 0, n - 1);
 	}
 
-}
